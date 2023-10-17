eda52d2313e72b9408616c1de71491c203d89e6e
Timofejj.Shovkun <shovkyntimofej@gmail.com>
2023-10-17 19:19:10 +0300

    add new class

--- src/test/java/com/Timofejj12/Runner.java
+++ src/test/java/com/Timofejj12/Runner.java
@@ -1,7 +1,17 @@
 package com.Timofejj12;
 
 public class Runner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        if (args.length < 3) {
+            System.out.println("Недостатньо аргументів. Використання: java -jar myApp.jar command filePath key");
+            return;
+        }
 
+        String command = args[0];
+        String filePath = args[1];
+        int key = Integer.parseInt(args[2]);
+
+        CLI cli = new CLI();
+        cli.runCommand(command, filePath, key);
     }
 }
